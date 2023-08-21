b512d7aa1fd0076134fb8311c6afada0f8f26234
Kostadin Kostadinov <kostadinovk2001@gmail.com>
2023-08-21 20:02:56 +0300

    Add react skeleton, scss, boostrap, header, and home page and footer unfinished components

--- FE/superlaw/src/App.tsx
+++ FE/superlaw/src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HomePage from './components/HomePage';
+import Login from './components/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <>
+      <Router>
+          <Header/>
+          <Routes>
+              <Route path='/' Component={HomePage}/>
+              <Route path='/login' Component={Login}/>
+          </Routes>
+          <Footer/>
+      </Router>
+  </>
+);
 
 export default App;
