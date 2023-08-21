3383e4c0a53470b809d9c15bd019aa30f99917f1
Kostadin Kostadinov <kostadinovk2001@gmail.com>
2023-08-21 21:56:30 +0300

    Finish the home page

--- FE/superlaw/src/components/HomePage/HomePage.tsx
+++ FE/superlaw/src/components/HomePage/HomePage.tsx
@@ -1,5 +1,14 @@
+import './HomePage.scss';
+
 const HomePage = () => {
-  return <h1>HomePage</h1>;
+  return (
+    <section className='cta'>
+      <div className='text'>
+        <h4>Намерете адвокат и запазете час за консултация онлайн</h4>
+        <p>През нашата платформа работят адвокати от цялата страна, търсете по специалност, град или кантора.</p>
+      </div>
+    </section>
+  );
 };
 
 export default HomePage;
