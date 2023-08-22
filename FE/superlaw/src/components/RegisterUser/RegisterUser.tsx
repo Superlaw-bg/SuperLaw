1d6ef2ee884c4218da4af91a0ff126491a9a6a3d
Kostadin Kostadinov <kostadinovk2001@gmail.com>
2023-08-22 09:19:05 +0300

    Add register user form

--- FE/superlaw/src/components/RegisterUser/RegisterUser.tsx
+++ FE/superlaw/src/components/RegisterUser/RegisterUser.tsx
@@ -1,3 +1,4 @@
+import './RegisterUser.scss';
 import { Button } from 'react-bootstrap';
 
 const Register = () => {
@@ -5,21 +6,54 @@ const Register = () => {
       <div className='wrap'>
         <div className='row'>
           <div className='form-wrapper col-md-4'>
-            <form className='login-form'>
-              <h1 className='login-heading'>Registraciq</h1>
+            <form className='register-form'>
+              <h1 className='register-heading'>Регистрирай се</h1>
               <div className='form-group'>
-                <label>Имейл</label>
-                <input type='text' className='form-control'/>
+                <label htmlFor='name'>Име</label>
+                <input id='name' type='text' className='form-control'/>
               </div>
 
               <div className='form-group'>
-                <label>Парола</label>
-                <input type='password' className='form-control'/>
+                <label htmlFor='surname'>Фамилия</label>
+                <input id='surname' type='text' className='form-control'/>
               </div>
 
-          <Button className='login-btn' type='submit' variant='primary'>Влез</Button>
-          <h5 className='link'>Нямаш акаунт?</h5>
-          <a className='link'>Регистрирай се</a>
+              <div className='form-group selection'>
+                <label htmlFor='region'>Област</label>
+                <select className='form-select' name='region' id='region'>
+                  <option selected disabled value='none'>Изберете област</option>
+                  <option value='sofia'>София град</option>
+                  <option value='sofia-region'>София област</option>
+                  <option value='plovdiv'>Пловдив</option>
+                  <option value='varna'>Варна</option>
+                  <option value='other'>Друга</option>
+                </select>
+              </div>
+
+              <div className='form-group'>
+                <label htmlFor='phone'>Телефон</label>
+                <div className='phone'>
+                  <p className='code form-control'>+359</p>
+                  <input type='text' id='phone' className='form-control'/>
+                </div>
+              </div>
+
+              <div className='form-group'>
+                <label htmlFor='email'>Имейл</label>
+                <input id='email' type='text' className='form-control'/>
+              </div>
+
+              <div className='form-group'>
+                <label htmlFor='password'>Парола</label>
+                <input id='password' type='password' className='form-control'/>
+              </div>
+
+              <div className='form-group'>
+                <label htmlFor='repeat-password'>Повтори Парола</label>
+                <input id='repeat-password' type='password' className='form-control'/>
+              </div>
+
+              <Button className='register-btn' type='submit' variant='primary'>Регистрация</Button>
             </form>
           </div>
         </div>
