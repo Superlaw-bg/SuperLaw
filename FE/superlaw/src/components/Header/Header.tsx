48ac0e7130f9cb4bdf01b34e0f23c237bf978119
Kostadin Kostadinov <kostadinovk2001@gmail.com>
2023-09-07 10:12:03 +0300

    Add logout

--- FE/superlaw/src/components/Header/Header.tsx
+++ FE/superlaw/src/components/Header/Header.tsx
@@ -1,19 +1,60 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Nav } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
 
 import './Header.scss';
+import { useStoreActions, useStoreState } from '../../store/hooks';
+import User from '../../store/auth/models/User';
+import toastService from '../../services/toastService';
+import { Lawyer } from '../../constants/roles';
 
 const Header = () => {
+    const navigate = useNavigate();
+    const { isLoggedIn, email, role } = useStoreState(store => store.auth.user);
+    const dispatchLogout = useStoreActions(actions => actions.auth.logout);
+
+    const logout = () => {
+        const user: User = {
+            isLoggedIn: false,
+            role: '',
+            token: '',
+            id: '',
+            email: ''
+        };
+
+        dispatchLogout(user);
+        toastService.showSuccess('Успешно илзязохте от акаунта си');
+        navigate('/');
+    };
+
+    const navigateToProfile = () => {
+        if (role === Lawyer) {
+            navigate('/profile');
+        } else {
+            navigate('/');
+        }
+    };
+
     return(
         <Navbar className='nav d-flex justify-content-between'>
             <div className='left'>
                 <Navbar.Brand as={Link} to="/" className='logo'>SuperLaw</Navbar.Brand>
             </div>
             <div className='right'>
-            <Nav.Item>
-                <Nav.Link as={Link} className="login nav-link" to="/login">Вход</Nav.Link>
-            </Nav.Item>
+                {isLoggedIn &&
+                    <div>
+                        <a className='email' onClick={navigateToProfile}>{email}</a>
+                        <Nav.Item>
+                            <Nav.Link className="logout nav-link" onClick={logout}>Изход</Nav.Link>
+                        </Nav.Item>
+                    </div>
+                }
+
+                {!isLoggedIn &&
+                    <Nav.Item>
+                        <Nav.Link as={Link} className="login nav-link" to="/login">Вход</Nav.Link>
+                    </Nav.Item> 
+                }
             </div>
         </Navbar>
     );
