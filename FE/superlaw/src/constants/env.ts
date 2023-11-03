59defe0ea09c112399d40b8d6f6bf26865bf9cdd
Kostadin Kostadinov <kostadinovk2001@gmail.com>
2023-11-03 17:54:12 +0200

    Set appsetting for prod

--- FE/superlaw/src/constants/env.ts
+++ FE/superlaw/src/constants/env.ts
@@ -3,6 +3,8 @@ const getApiBaseUrl = () => {
 
     if (process.env.NODE_ENV === 'development') {
         url = 'https://localhost:44350/api/';
+    } else if(process.env.NODE_ENV === 'production') {
+        url = 'https://superlawapiprod.azurewebsites.net/api/';
     } else {
         url = 'https://superlawapi.azurewebsites.net/api/';
     }
