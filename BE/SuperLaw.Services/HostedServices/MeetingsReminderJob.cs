db7cb0f4494ae0adb84acf353f40de4377fb1e0b
Kostadin Kostadinov <kostadinovk2001@gmail.com>
2023-12-28 11:05:03 +0200

    Change logs

--- BE/SuperLaw.Services/HostedServices/MeetingsReminderJob.cs
+++ BE/SuperLaw.Services/HostedServices/MeetingsReminderJob.cs
@@ -26,7 +26,7 @@ namespace SuperLaw.Services.HostedServices
 
         public Task Execute(IJobExecutionContext context)
         {
-            _logger.LogTrace($"Meetings Reminder Job started at: {DateTime.UtcNow}");
+            _logger.LogWarning($"Meetings Reminder Job started at: {DateTime.UtcNow}");
 
             var tomorrowDate = DateTime.UtcNow.AddDays(1).Date;
 
@@ -37,7 +37,7 @@ namespace SuperLaw.Services.HostedServices
                 .ThenInclude(x => x.Client)
                 .ToList();
 
-            _logger.LogTrace($"Meetings Reminder Job - There are {lawyersWithMeetings.Count} lawyers with meetings tomorrow");
+            _logger.LogWarning($"Meetings Reminder Job - There are {lawyersWithMeetings.Count} lawyers with meetings tomorrow");
 
             var usersWithMeetings = _context.Users
                 .Where(x => x.Meetings.Any(m => m.DateTime.Date == tomorrowDate))
@@ -46,7 +46,7 @@ namespace SuperLaw.Services.HostedServices
                 .ThenInclude(x => x.User)
                 .ToList();
 
-            _logger.LogTrace($"Meetings Reminder Job - There are {usersWithMeetings.Count} users with meetings tomorrow");
+            _logger.LogWarning($"Meetings Reminder Job - There are {usersWithMeetings.Count} users with meetings tomorrow");
 
             foreach (var lawyer in lawyersWithMeetings)
             {
@@ -93,8 +93,8 @@ namespace SuperLaw.Services.HostedServices
 
                 _emailService.SendEmail(user.Email, $"Предстоящи консултации {tomorrowDate:dd.MM.yy}", sb.ToString());
             }
-            
-            _logger.LogTrace($"Meetings Reminder Job finished at: {DateTime.UtcNow}");
+
+            _logger.LogWarning($"Meetings Reminder Job finished at: {DateTime.UtcNow}");
             return Task.CompletedTask;
         }
     }
